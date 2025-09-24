00bd11077e5a7a992dec56d58a3503bbec7dae30
syukrisolihin <syukrisolihin7@gmail.com>
2025-09-24 16:52:27 +0800

    feat: Build initial launch pad page with Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'bg-primary': '#181818',
+				'text-primary': '#F5F5F5',
+				'text-secondary': '#a0a0a0',
+				'accent-blue': '#00BFFF',
+				'accent-amber': '#B58500',
+			},
+			fontFamily: {
+				'heading': ['Playfair Display', ...defaultTheme.fontFamily.serif],
+				'body': ['Montserrat', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+}
